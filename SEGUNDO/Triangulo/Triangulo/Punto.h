127404eb815ca5a3bcb54f757cde4b0eddd6cdc4
Steven Vargas González <65059118+StevenVargas04@users.noreply.github.com>
2020-07-20 00:57:56 -0500

    Add files via upload

---
+++ SEGUNDO/Triangulo/Triangulo/Punto.h
@@ -0,0 +1,35 @@
+/*
+  Nombre: Punto.h
+  Autor: IPOO - Univalle Tulua
+  Fecha Creacion: 2020
+  Fecha Modificaci�n: 2020
+  Version : 1.0
+  Email: suMail
+  */
+/*
+   Clase: Punto
+   Responsabilidad:
+    - Simula ser un punto  del plano cartesiano,de coordenadas (x,y).
+    - A traves de ella, podemos crear, cambiar y devolver puntos en el plano cartesiano.
+   Colaboracion: ninguna
+*/
+
+#ifndef __CLASSPUNTO
+#define __CLASSPUNTO
+class Punto
+{
+  private:
+     int x;
+     int y;
+  public:
+
+  Punto();
+  Punto(int dX, int dY);
+//~Punto();
+	int darX();
+	int darY();
+	void cambiarX(int x1);
+	void cambiarY(int y1);
+  double colocarPuntos(int x,int y);
+};
+#endif //CLASPUNTO
